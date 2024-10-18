ab3cb471bb500f920734ffbf27d319c0b51a72e0
tomburger64 <tom64bonde@gmail.com>
2024-10-18 10:55:10 +0200

    form edit

--- meme-generator/src/components/meme.jsx
+++ meme-generator/src/components/meme.jsx
@@ -5,9 +5,14 @@ export default function Meme(){
         <div className='form-bg w-100'>
             <form action="" className='form d-flex flex-column p-5 gap-5'>
                 <div className="d-flex justify-content-between">
-                    <input className="p-3" type="text" placeholder='Top line' />
-
-                    <input className="p-3" type="text" placeholder='Bottom line'/>
+                    <div className="d-flex flex-column">
+                        <label htmlFor="">Top caption</label>
+                        <input className="p-3" type="text" placeholder='MFW (my face when)' />
+                    </div>
+                    <div className="d-flex flex-column">
+                        <label htmlFor="">Bottom caption</label>
+                        <input className="p-3" type="text" placeholder='Ultrakill speedruns'/>
+                    </div>
                 </div>
 
                 <button className='d-flex justify-content-center align-items-center p-3'>Get a new meme</button>
