52aa15ab281c3eadbf77012185bce8f420ce5666
tomburger64 <tom64bonde@gmail.com>
2024-10-22 12:09:42 +0200

    finally heard the word "states" in the course

--- meme-generator/src/components/meme.jsx
+++ meme-generator/src/components/meme.jsx
@@ -1,3 +1,5 @@
+import React from 'react';
+
 import './style/meme.css';
 
 import memesData from '../fakeApiData';
@@ -18,23 +20,26 @@ function randomMemes(){
     // calling random url using the random int as index
 };
 
-// exercise "break" (still inside the course)
-// https://v2.scrimba.com/learn-react-c0e/~029
-let thingsArray = ["Thing 1", "Thing 2"]
-const thingsElements = thingsArray.map(thing => <p key={thing}>{thing}</p>)
-function addThing(){
-    thingsArray.push("Thing " + (thingsArray.length + 1))
-    console.log(thingsArray)
-    event.preventDefault(); //deprecated but idc it's just for here and now
-}
-// also added a button on top that triggers the function (will be deleted once the exercise is done)
-// - - - - -
-
 export default function MemeGen(){
+
+    // exercise "break" (still inside the course)
+    // https://v2.scrimba.com/learn-react-c0e/~029
+        const [things, setThings] = React.useState(["Thing 1", "Thing 2"])
+    
+    function addItem() {
+        const newThingText = `Thing ${things.length + 1}`
+        setThings(prevState => [...prevState, newThingText]);
+        event.preventDefault(); //deprecated but it's just for here and now
+    }
+    
+    const thingsElements = things.map(thing => <p key={thing}>{thing}</p>)
+    // also added a button on top that triggers the function (will be deleted once the exercise is done)
+    // - - - - -
+
     return(
         <div className='form-bg w-100'>
             <form action="" className='form d-flex flex-column p-5 gap-5'>
-                <button onClick={addThing}>Click to add thing</button>
+                <button onClick={addItem}>Click to add thing</button>
                 {thingsElements}
                 <p>{randomMeme}</p>
                 <div className="d-flex justify-content-between">
