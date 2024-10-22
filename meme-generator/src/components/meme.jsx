b06811a827bd8e9f6531a900a95fc2596806cd3e
tomburger64 <tom64bonde@gmail.com>
2024-10-22 12:33:19 +0200

    exercise done

--- meme-generator/src/components/meme.jsx
+++ meme-generator/src/components/meme.jsx
@@ -29,12 +29,61 @@ export default function MemeGen(){
     function addItem() {
         const newThingText = `Thing ${things.length + 1}`
         setThings(prevState => [...prevState, newThingText]);
-        event.preventDefault(); //deprecated but it's just for here and now
+        event.preventDefault();
     }
     
     const thingsElements = things.map(thing => <p key={thing}>{thing}</p>)
     // also added a button on top that triggers the function (will be deleted once the exercise is done)
     // - - - - -
+
+
+
+    // other exercise (introduction to states)
+    /*
+    Challenge: complete the function below
+    Given a name, return "Good <timeOfDay>, <name>!" depending
+    on the current time of day.
+
+    4 am - 11:59 am, timeOfDay = "morning"
+    12 pm - 4:59 pm: timeOfDay = "afternoon"
+    5 pm - 7:59 pm: timeOfDay = "evening"
+    8 pm - 3:59 am: timeOfDay = "night"
+
+    E.g. if it's currently 1 pm, greeting("Jane") ==> "Good afternoon, Jane!"
+
+    Hint: you can get the current time of day by using: 
+
+    const date = new Date()
+    const hours = date.getHours()
+    */
+    const date = new Date()
+    const hours = date.getHours()
+
+    function greeting(name) {
+        let timeOfDay
+
+        if (hours >= 4 && hours <= 11.9){
+            timeOfDay = "morning"
+            console.log(`Good ${timeOfDay}, ${name}`)
+        }
+        else if (hours >= 12 && hours <= 16.9){
+            timeOfDay = "afternoon"
+            console.log(`Good ${timeOfDay}, ${name}`)
+        }
+        else if (hours >= 17 && hours <= 19.9
+        ){
+            timeOfDay = "evening"
+            console.log(`Good ${timeOfDay}, ${name}`)
+        }
+        else if (hours >= 20 && hours <= 3.9){
+            timeOfDay = "night"
+            console.log(`Good ${timeOfDay}, ${name}`)
+        }
+    }
+    greeting("Bob")
+
+
+
 
     return(
         <div className='form-bg w-100'>
