c27088c37de26591ff5a3b4cf78000bb6b82a155
tomburger64 <tom64bonde@gmail.com>
2024-10-24 11:35:11 +0200

    counter exercsise using states (+ edits)

--- meme-generator/src/App.jsx
+++ meme-generator/src/App.jsx
@@ -1,15 +1,16 @@
-import { useState } from 'react';
 import './App.css';
 
 // comps
 import Header from './components/header';
-import MemeGen from './components/meme'
+import MemeGen from './components/meme';
+import Counter from './components/simpleCount';
 
 export default function App() {
   return (
     <div className="container d-flex justify-content-center align-items-center flex-column">
       <Header />
       <MemeGen />
+      <Counter />
     </div>
   )
 };
---
+++ meme-generator/src/components/simpleCount.jsx
@@ -0,0 +1,28 @@
+// https://v2.scrimba.com/learn-react-c0e/~02g
+// course exercise, off the original project
+
+import React from "react"
+
+import "./style/counter.css"
+
+export default function Counter() {
+    const [count, setCount] = React.useState(0)
+    
+    function add() {
+        setCount(count + 1)
+    }
+    
+    function subtract() {
+        setCount(count - 1)
+    }
+    
+    return (
+        <div className="counter">
+            <button className="counter--minus" onClick={subtract}>–</button>
+            <div className="counter--count">
+                <h1>{count}</h1>
+            </div>
+            <button className="counter--plus" onClick={add}>+</button>
+        </div>
+    )
+}
