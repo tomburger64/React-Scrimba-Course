77d3f5879c2357ac323983c9de91a5b629fcabb6
tomburger64 <tom64bonde@gmail.com>
2024-10-25 11:38:11 +0200

    displaying random meme onclick (struggling)

--- meme-generator/src/App.jsx
+++ meme-generator/src/App.jsx
@@ -3,14 +3,14 @@ import './App.css';
 // comps
 import Header from './components/header';
 import MemeGen from './components/meme';
-import Counter from './components/simpleCount'; // ← off cousre exercise, you can uncomment it if you want
+//import Counter from './components/simpleCount'; // ← off cousre exercise, you can uncomment it if you want
 
 export default function App() {
   return (
     <div className="container d-flex justify-content-center align-items-center flex-column">
       <Header />
       <MemeGen />
-      <Counter />
+      {/* <Counter /> */}
     </div>
   )
 };
--- meme-generator/src/components/meme.jsx
+++ meme-generator/src/components/meme.jsx
@@ -7,38 +7,38 @@ import './style/meme.css';
 import memesData from '../fakeApiData';
 
 
-let randomMeme
+let randomMeme;
 function randomMemes(){
     const urls = memesData.data.memes.map(meme => meme);
     // get all the urls
 
     function getRandomInt(max) {
-        return Math.floor(Math.random() * max)
+        return Math.floor(Math.random() * max);
     };
     // get a random int
 
     let randomInt = getRandomInt(urls.length);
-    randomMeme = urls[randomInt]
+    randomMeme = urls[randomInt];
     // calling random url using the random int as index
+
+    console.log(randomMeme);
 };
 
 export default function MemeGen(){
 
     // exercise "break" (still inside the course)
     // https://v2.scrimba.com/learn-react-c0e/~029
-    const [things, setThings] = React.useState(["Thing 1", "Thing 2"])
+    // const [things, setThings] = React.useState(["Thing 1", "Thing 2"])
     
-    function addItem() {
-        const newThingText = `Thing ${things.length + 1}`
-        setThings(prevState => [...prevState, newThingText]);
-        event.preventDefault();
-    }
+    // function addItem() {
+    //     const newThingText = `Thing ${things.length + 1}`
+    //     setThings(prevState => [...prevState, newThingText]);
+    //     event.preventDefault();
+    // }
     
-    const thingsElements = things.map(thing => <p key={thing}>{thing}</p>)
+    // const thingsElements = things.map(thing => <p key={thing}>{thing}</p>)
     // also added a button on top that triggers the function (will be deleted once the exercise is done)
     // - - - - -
-
-
 
     // other exercise (introduction to states)
     // https://v2.scrimba.com/learn-react-c0e/~02b
@@ -81,15 +81,20 @@ export default function MemeGen(){
     //     console.log(`Good ${timeOfDay}, ${name}`);
     // };
     // greeting("Bob");
+    // - - - - -
 
 
 
+    const [memeImg, setMemeImg] = React.useState('');
+    function getUrl(){
+        setMemeImg(prevMemeImg => randomMeme);
+    };
 
     return(
         <div className='form-bg w-100'>
             <form action="" className='form d-flex flex-column p-5 gap-5'>
-                <button onClick={addItem}>Click to add thing</button>
-                {thingsElements}
+                {/* <button onClick={addItem}>Click to add thing</button>
+                {thingsElements} */}
                 <p>{randomMeme}</p>
                 <div className="d-flex justify-content-between">
                     
@@ -104,6 +109,7 @@ export default function MemeGen(){
 
                 <button className='d-flex justify-content-center align-items-center p-3' onClick={randomMemes} type='button'>Get a new meme</button>
             </form>
+            <div className="meme-container"><img src={memeImg} alt="" /></div>
         </div>
     )
 }
