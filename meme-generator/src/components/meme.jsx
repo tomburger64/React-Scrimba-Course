e5af1df9980a580b8e345e75e5f00b14ba9d07d6
tomburger64 <tom64bonde@gmail.com>
2024-10-26 15:35:51 +0200

    ternary exercises (from course)

--- meme-generator/src/components/meme.jsx
+++ meme-generator/src/components/meme.jsx
@@ -69,8 +69,63 @@ export default function MemeGen(){
 
 
 
+    // quick ternary exercise (from the course)
+    // export default function App() {
+    //     /**
+    //      * Challenge: Replace the if/else below with a ternary
+    //      * to determine the text that should display on the page
+    //      */
+    //     const isGoingOut = false
+    // 
+    //     function answer(){
+    //         return isGoingOut ? "Yes" : "No" // Use ternary here
+    //     }
+    // 
+    //     return (
+    //         <div className="state">
+    //             <h1 className="state--title">Do I feel like going out tonight?</h1>
+    //             <div className="state--value">
+    //                 <h1>{answer()}</h1> {/* add '()' for the func to run automatically*/}
+    //             </div>
+    //         </div>
+    //     )
+    // }
+    // 
+    // works but the course's solution is better:
+    // let answer = isGoingOut ? "Yes" : "No" (also don't add '()' when calling the js in the jsx)
+    // 
+    // 
+    // 
+    // second exercise for ternary op (put the ternary directly in the js call)
+    // const isGoingOut = true
+    // let answer
+    // 
+    // return (
+    //     <div className="state">
+    //         <h1 className="state--title">Do I feel like going out tonight?</h1>
+    //         <div className="state--value">
+    //             <h1>{answer = isGoingOut ? "Yes" : "No"}</h1>
+    //         </div>
+    //     </div>
+    // )
+    // 
+    // again, works but solution is better (directly taking the var's value):
+    // const isGoingOut = true
+    // 
+    // return (
+    //     <div className="state">
+    //         <h1 className="state--title">Do I feel like going out tonight?</h1>
+    //         <div className="state--value">
+    //             <h1>{isGoingOut ? "Yes" : "No"}</h1>
+    //         </div>
+    //     </div>
+    // )
+    // - - - - -
+
+
+
     // reminder to self (considering how my code was before I had to "cheat"):
-    // use the state directly, not sopme variable that's indirectly linked to the state unless you want a headache
+    // use the state directly, not through some variable that's indirectly linked to the state unless you want a headache
     // - tom
 
 
