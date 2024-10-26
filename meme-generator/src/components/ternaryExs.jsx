b1ab7574ae2cc6f9f50d65659834a3005e49b681
tomburger64 <tom64bonde@gmail.com>
2024-10-26 16:32:13 +0200

    done

--- meme-generator/src/components/ternaryExs.jsx
+++ meme-generator/src/components/ternaryExs.jsx
@@ -1,3 +1,6 @@
+// states
+import React from 'react';
+
 // css
 import './style/ternary-exs.css';
 
@@ -9,12 +12,26 @@ export default function TernaryExs(){
      *   boolean value (true -> false, false -> true)
      * - Display "Yes" if `isGoingOut` is `true`, "No" otherwise
      */
+
+    const [isGoingOut, setIsGoingOut] = React.useState(true);
+
+    function setAnswer(e){
+        e.preventDefault();
+
+        setIsGoingOut(prevState => {
+            prevState = !prevState;
+            prevState = !prevState;
+            return prevState
+        });
+    };
     
+
     return(
         <div className="state">
             <h1 className="state--title">Do I feel like going out tonight?</h1>
-            <div className="state--value">
-                <h1>Yes</h1>
+            <div className="state--value" onClick={setAnswer}>
+                <h1 className='text-center'>{isGoingOut ? "Yes" : "No"}</h1>
+                {/* clicking just inverts the answer */}
             </div>
         </div>
     )
