864c2c1fc2194781cc8c6839af1d5b51bafb9fc8
tomburger64 <tom64bonde@gmail.com>
2024-10-29 11:37:33 +0100

    new challenge & old exercise sorted

--- meme-generator/src/components/meme.jsx
+++ meme-generator/src/components/meme.jsx
@@ -10,54 +10,26 @@ import memesData from '../fakeApiData';
 
 export default function MemeGen(){
 
-    // other exercise (introduction to states)
-    // https://v2.scrimba.com/learn-react-c0e/~02b
-    /*
-    Challenge: complete the function below
-    Given a name, return "Good <timeOfDay>, <name>!" depending
-    on the current time of day.
-
-    4 am - 11:59 am, timeOfDay = "morning"
-    12 pm - 4:59 pm: timeOfDay = "afternoon"
-    5 pm - 7:59 pm: timeOfDay = "evening"
-    8 pm - 3:59 am: timeOfDay = "night"
-
-    E.g. if it's currently 1 pm, greeting("Jane") ==> "Good afternoon, Jane!"
-
-    Hint: you can get the current time of day by using: 
-
-    const date = new Date()
-    const hours = date.getHours()
-    */
-    // const date = new Date();
-    // const hours = date.getHours();
-
-    // function greeting(name) {
-    //     let timeOfDay;
-
-    //     if (hours >= 4 && hours <= 11){
-    //         timeOfDay = "morning";
-    //     }
-    //     else if (hours >= 12 && hours <= 16){
-    //         timeOfDay = "afternoon";
-    //     }
-    //     else if (hours >= 17 && hours <= 19
-    //     ){
-    //         timeOfDay = "evening";
-    //     }
-    //     else if (hours >= 20 && hours <= 3.9){
-    //         timeOfDay = "night";
-    //     }
-    //     console.log(`Good ${timeOfDay}, ${name}`);
-    // };
-    // greeting("Bob");
-    // - - - - -
-
     // reminder to self (considering how my code was before I had to "cheat"):
     // use the state directly, not through some variable that's indirectly linked to the state unless you want a headache
     // - tom
 
-
+    /**
+     * Challenge: Update our state to save the meme-related
+     * data as an object called `meme`. It should have the
+     * following 3 properties:
+     * topText, bottomText, randomImage.
+     * 
+     * The 2 text states can default to empty strings for now,
+     * amd randomImage should default to "http://i.imgflip.com/1bij.jpg"
+     * 
+     * Next, create a new state variable called `allMemeImages`
+     * which will default to `memesData`, which we imported above
+     * 
+     * Lastly, update the `getMemeImage` function and the markup 
+     * to reflect our newly reformed state object and array in the
+     * correct way.
+     */
 
     // meme img state
     // apparently leaving an empty string gives an error (I don't see it anywhere though, so I'll leave it)
