fc1e09579aeb3d03500c76c6b2e412b5f114b099
tomburger64 <tom64bonde@gmail.com>
2024-10-31 11:28:43 +0100

    back to count component (challenge)

--- meme-generator/src/App.jsx
+++ meme-generator/src/App.jsx
@@ -3,7 +3,7 @@ import './App.css';
 // comps
 import Header from './components/header';
 import MemeGen from './components/meme';
-// import Counter from './components/simpleCount'; // ← appart exercise, you can uncomment it if you want
+import Counter from './components/simpleCount'; // ← appart exercise, you can uncomment it if you want
 // import TernaryExs from './components/ternaryExs'; // ← appart exercise, you can uncomment it if you want
 // import ThingsExs from './components/thingsExs'; // ← appart exercise, you can uncomment it if you want
 // import ContactCard from './components/contactCardExs'; // ← appart exercise, you can uncomment it if you want
@@ -13,7 +13,7 @@ export default function App() {
     <div className="container d-flex justify-content-center align-items-center flex-column">
       <Header />
       <MemeGen />
-      {/* <Counter /> */}
+      <Counter />
       {/* <Ternary /> */}
       {/* <ThingsExs /> */}
       {/* <ContactCard /> */}
