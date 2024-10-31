a414f65988dff5ab42b2127b2ffb846061172309
tomburger64 <tom64bonde@gmail.com>
2024-10-31 11:30:45 +0100

    nvm new file

--- meme-generator/src/components/simpleCount.jsx
+++ meme-generator/src/components/simpleCount.jsx
@@ -15,18 +15,6 @@ export default function Counter() {
     function subtract() {
         setCount(prevCount => prevCount - 1);
     };
-    
-    /**
-     * https://v2.scrimba.com/learn-react-c0e/~02r
-     * Challenge:
-     * - Create a new component named Count
-     *    - It should receive a prop called `number`, whose value
-     *      is the current value of our count
-     *    - Have the component render the whole div.counter--count
-     *      and display the incoming prop `number`
-     * - Replace the div.counter--count below with an instance of
-     *   the new Count component
-     */
 
     return (
         <div className="counter">
