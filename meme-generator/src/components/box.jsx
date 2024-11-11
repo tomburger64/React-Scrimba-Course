066251f67fff1513e3602a9b768e1bd2b2c5dc41
tomburger64 <tom64bonde@gmail.com>
2024-11-11 13:58:43 +0100

    concerning the ano arrow func in box.jsx

--- meme-generator/src/components/box.jsx
+++ meme-generator/src/components/box.jsx
@@ -8,9 +8,16 @@ export default function Box(props){
     return(
         <>
         {/* indirectly passing the toggle func as an anon arrow function
-         * this allows to pass the id prop as a parameter
+         * this allows to pass the id prop as an argument
          * as it gets rendered inside the parent component with the correct id
          * (key doesn't work because it's a unique kind of prop, hence the id prop)
+         * 
+         * specifically concerning the ano arrowfunction,
+         * it's because of the way js runs (according to chatgpt)
+         * 
+         * basically unless I fall for it another 100 times more
+         * I'll need to ask AI why it doesn't work
+         * or else I'll just get a headache jackpot
          */}
             <button 
             className='box' 
